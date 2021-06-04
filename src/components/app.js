35bff691d85034c0190e16f4966f2e0205efdd9a
HwaGyeong <dlghkrud18@naver.com>
2021-06-04 16:15:30 +0900

    hot loader 적용

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,24 @@
 import React from 'react';
 
-class App extends React.Component{
-    render()
-    {
-        return (
-          <h1>Hello world
-          </h1>  
-        );
-    }
+class App extends React.Component {
 
+  constructor(props) {
+      super(props);
+      this.state = {
+          value: 0
+      }
+  }
+
+  render(){
+
+      return (
+          <div>
+              <h1>Hello React!!!!!!</h1>
+              <h2>{this.state.value}</h2>
+              <button onClick={() => { this.setState({value: this.state.value + 1}) }}>CLICK ME</button>
+          </div>
+      );
+  }
 }
 
 export default App;
